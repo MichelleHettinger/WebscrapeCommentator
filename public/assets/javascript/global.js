b1cdfbb3979c56620bf36d0b705111f7d70de9a6
Michelle Hettinger <Michelle Hettinger>
2016-10-28 00:59:13 -0400

    deleted everything related to express-handlebars. brought routes into server.js file and removed the controllers folder. title and summarry now appear when clicking through articles

--- public/assets/javascript/global.js
+++ public/assets/javascript/global.js
@@ -4,18 +4,6 @@
 // when the document is ready
 $(document).ready(function() {
 
-  // immediately hide the container class
-  $('.container').hide();
-
-  // // run the fetch data function, which will scrape our data
-  // fetchData();
-
-  // hide these sections before anything else
-  $("#seek-box").hide();
-  $("#input-area").hide();
-  $("#saved-text").hide();
-  $("#saved-area").hide();
-
   // when the visitor clicks on the #seek-box
   $("#seek-box").click(function() {
 
@@ -23,12 +11,15 @@ $(document).ready(function() {
     // and populate the proper sections.
     populate();
 
-    // with that done, reveal the container
-    $('.container').show();
-
     // hide the initial seek box from view
     $("#seek-box").hide();
+
   });
+
+
+
+
+
 
 });
 
@@ -46,178 +37,52 @@ var sideAry = ['back', 'right', 'left', 'top', 'bottom', 'front'];
 var populate = function() {
 
   // jQuery AJAX call for JSON to grab all articles scraped to our db
-  $.getJSON('/check', function(data) {
+  $.getJSON('/articles', function(data) {
 
     // save the articles object data to our mongoData variable
     mongoData = data;
+
+    console.log(data);
 
     // save the latest article's date and save it to dataDate
     dataDate = mongoData[mongoData.length - 1].date;
   })
   // when that's done
   .done(function() {
-    // running clickBox and saveNote functions
+    // running clickBox and saveComment functions
     clickBox();
-    saveNote();
+    saveComment();
   });
 };
 
-// ajax get notes data
-var gather = function() {
-
-  // find the article's current id
-  var idCount = dataCount - 1;
-
-  // jQuery AJAX call for JSON data of notes
-  $.ajax({
-    type: "POST",
-    dataType: "json",
-    url: '/gather',
-    data: {
-      id: mongoData[idCount]._id
+// This function handles what happens when the cube is clicked
+var clickBox = function() {
+  $("#cube").on("click", function() {
+    // rotate cycle
+    if (state <= 5) {
+      state++;
+    } else {
+      state = 0;
     }
-  })
+    // add the proper states to the cube based on where it's clicked
+    $('#cube').removeClass().addClass(cubeRotateAry[state]);
 
-  // with that done, post the current Notes to the page
-  .done(function(currentNotes) {
-    postNote(currentNotes);
-  })
+    //animate headline
+    headline();
 
-  // if something went wrong, tell the user
-  .fail(function() {
-    console.log("Sorry. Server unavailable.");
+    //animate text
+    typeIt();
+
+    //render comments
+    gather();
+
+    //enable delete click listener
+    deleteComment();
+
+    //show the comment boxes
+    $("#input-area").show();
+    $("#saved-area").show();
   });
-};
-
-// render notes from data in the last function
-var postNote = function(currentNotes) {
-
-  // remove inputs from the note box
-  $("#note-box").val("");
-
-  // make an empty placeholder var for a note
-  var note = "";
-
-  // for each of the notes
-  for (var i = 0; i < currentNotes.length; i++) {
-
-    // make the note variable equal to itself, 
-    // plus the new note and a new line
-    note = note + currentNotes[i].noteText + '\n';
-  }
-  // put the current collection of notes into the notebox
-  $("#note-box").val(note);
-};
-
-// function containing listener to save notes and clear note taking area
-var saveNote = function() {
-
-  // when someone clicks the note button
-  $("#note-button").on('click', function() {
-
-    // grab the value from the input box
-    var text = $("#input-box").val();
-
-    // grab the current article's id
-    var idCount = dataCount - 1;
-
-    // ajax call to save the note
-    $.ajax({
-      type: "POST",
-      dataType: "json",
-      url: '/save',
-      data: {
-        id: mongoData[idCount]._id, // article id
-        date: dataDate, // date of article's last update
-        note: text // date of note
-      }
-    })
-    // with that done
-    .done(function() {
-
-      // empty the input box
-      $("#input-box").val("");
-
-      // grab the notes again because we just saved a new note
-      gather();
-    })
-    // if it fails, give the user an error message
-    .fail(function() {
-      console.log("Sorry. Server unavailable.");
-    });
-
-  });
-};
-
-// function containing listener to delete notes and clear note taking area
-var deleteNote = function() {
-
-  // when user clicks delete button
-  $("#delete-button").on('click', function() {
-
-    // make the idCount equal the current article
-    var idCount = dataCount - 1;
-
-    // send an ajax call to delete
-    $.ajax({
-      type: "DELETE",
-      dataType: "json",
-      url: '/delete',
-      data: {
-        id: mongoData[idCount]._id,
-      }
-    })
-    // with that done, empty the note-box input
-    .done(function() {
-      $("#note-box").val("");
-    })
-    // if it fails, tell the user
-    .fail(function() {
-      console.log("Sorry. Server unavailable.");
-    });
-
-  });
-};
-
-// This function handles typing animations
-var typeIt = function() {
-  $("#typewriter-headline").remove();
-  $("#typewriter-summary").remove();
-  var h = 0;
-  var s = 0;
-  var newsText;
-
-  if (state > 0) {
-    side = state - 1;
-  } else {
-    side = 5;
-  }
-
-  $("." + sideAry[side]).append("<div id='typewriter-headline'></div>");
-  $("." + sideAry[side]).append("<div id='typewriter-summary'></div>");
-
-  // cycle to different story
-  console.log(mongoData);
-  var headline = mongoData[dataCount].headline;
-  var summary = mongoData[dataCount].summary;
-  dataCount++;
-  // type animation for new summary
-  (function type() {
-    //console.log(newsText);
-    printHeadline = headline.slice(0, ++h);
-    printSummary = summary.slice(0, ++s);
-
-
-    // put in the text via javascript
-    $("#typewriter-headline").text(printHeadline);
-    $("#typewriter-summary").text(printSummary);
-
-    // return stop when text is equal to the writeTxt
-    if (printHeadline.length === headline.length && printSummary.length === summary.length) {
-      return;
-    }
-    setTimeout(type, 35);
-  }());
 };
 
 // render the headline headline
@@ -240,35 +105,176 @@ var headline = function() {
     });
 };
 
-// This function handles what happens when the cube is clicked
-var clickBox = function() {
-  $("#cube").on("click", function() {
-    // rotate cycle
-    if (state <= 5) {
-      state++;
-    } else {
-      state = 0;
+// This function handles typing animations
+var typeIt = function() {
+  $("#typewriter-headline").remove();
+  $("#typewriter-summary").remove();
+  var h = 0;
+  var s = 0;
+  var newsText;
+
+  if (state > 0) {
+    side = state - 1;
+  } else {
+    side = 5;
+  }
+
+  $("." + sideAry[side]).append("<div id='typewriter-headline'></div>");
+  $("." + sideAry[side]).append("<div id='typewriter-summary'></div>");
+
+  // cycle to different story
+  console.log(mongoData);
+  var headline = mongoData[dataCount].title;
+  var summary = mongoData[dataCount].summary;
+  dataCount++;
+  // type animation for new summary
+  (function type() {
+    //console.log(newsText);
+    printHeadline = headline.slice(0, ++h);
+    printSummary = summary.slice(0, ++s);
+
+
+    // put in the text via javascript
+    $("#typewriter-headline").text(printHeadline);
+    $("#typewriter-summary").text(printSummary);
+
+    // return stop when text is equal to the writeTxt
+    if (printHeadline.length === headline.length && printSummary.length === summary.length) {
+      return;
     }
-    // add the proper states to the cube based on where it's clicked
-    $('#cube').removeClass().addClass(cubeRotateAry[state]);
+    setTimeout(type, 35);
+  }());
+};
 
-    //animate headline
-    headline();
+// ajax get comments data
+var gather = function() {
 
-    //animate text
-    typeIt();
+  // find the article's current id
+  var idCount = dataCount - 1;
 
-    //render notes
-    gather();
+  // jQuery AJAX call for JSON data of comments
+  $.ajax({
+    type: "POST",
+    dataType: "json",
+    url: '/gather',
+    data: {
+      id: mongoData[idCount]._id
+    }
+  })
 
-    //enable delete click listener
-    deleteNote();
+  // with that done, post the current Comments to the page
+  .done(function(currentComments) {
+    postComment(currentComments);
+  })
 
-    //show the note boxes
-    $("#input-area").show();
-    $("#saved-area").show();
+  // if something went wrong, tell the user
+  .fail(function() {
+    console.log("Sorry. Server unavailable.");
   });
 };
+
+// function containing listener to save comments and clear comment taking area
+var saveComment = function() {
+
+  // when someone clicks the comment button
+  $("#comment-button").on('click', function() {
+
+    // grab the value from the input box
+    var text = $("#input-box").val();
+
+    // grab the current article's id
+    var idCount = dataCount - 1;
+
+    // ajax call to save the comment
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: '/save',
+      data: {
+        id: mongoData[idCount]._id, // article id
+        date: dataDate, // date of article's last update
+        comment: text // date of comment
+      }
+    })
+    // with that done
+    .done(function() {
+
+      // empty the input box
+      $("#input-box").val("");
+
+      // grab the comments again because we just saved a new comment
+      gather();
+    })
+    // if it fails, give the user an error message
+    .fail(function() {
+      console.log("Sorry. Server unavailable.");
+    });
+
+  });
+};
+
+
+// render comments from data in the last function
+var postComment = function(currentComments) {
+
+  // remove inputs from the comment box
+  $("#comment-box").val("");
+
+  // make an empty placeholder var for a comment
+  var comment = "";
+
+  // for each of the comments
+  for (var i = 0; i < currentComments.length; i++) {
+
+    // make the comment variable equal to itself, 
+    // plus the new comment and a new line
+    comment = comment + currentComments[i].commentText + '\n';
+  }
+  // put the current collection of comments into the commentbox
+  $("#comment-box").val(comment);
+};
+
+
+
+// function containing listener to delete comments and clear comment taking area
+var deleteComment = function() {
+
+  // when user clicks delete button
+  $("#delete-button").on('click', function() {
+
+    // make the idCount equal the current article
+    var idCount = dataCount - 1;
+
+    // send an ajax call to delete
+    $.ajax({
+      type: "DELETE",
+      dataType: "json",
+      url: '/delete',
+      data: {
+        id: mongoData[idCount]._id,
+      }
+    })
+    // with that done, empty the comment-box input
+    .done(function() {
+      $("#comment-box").val("");
+    })
+    // if it fails, tell the user
+    .fail(function() {
+      console.log("Sorry. Server unavailable.");
+    });
+
+  });
+};
+
+
+
+
+// Don't need to edit the functions below //
+
+
+
+
+
 
 // // ajax call to do the scrape
 // var fetchData = function() {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-//////////////////////////
+///////////////////////////
 ////// Dependencies //////
-//////////////////////////
+/////////////////////////
 
 //To run our server
 var express = require('express');
@@ -11,48 +11,30 @@ var PORT = process.env.PORT || 3000
 var bp = require('body-parser');
 var logger = require('morgan');
 
-//Render query data
-var exphbs = require('express-handlebars');
-
-//File containing all the routes
-var routes = require('./controllers/routecontroller.js');
-
 //Database configuration with mongoose
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/webscrapecomment');
 var db = mongoose.connection;
 
-//Pathing
-var path = require('path');
-
-
 // Schemas
 var Article = require('./models/Article.js');
-var Note = require('./models/Comment.js');
+var Comment = require('./models/Comment.js');
 
 //////////////////////////
 ////// Middleware ///////
-/////////////////////////
+////////////////////////
 
 //Parsing and logging request body
 app.use(bp.urlencoded({extended: false}))
 app.use(logger('dev'));
 
-//Set up handlebars
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
 //Enables access to public directory
 app.use(express.static('public'));
-// app.use(express.static(__dirname + '/public'));
-// app.use(express.static(path.join(__dirname,'public')));
 
-// console.log("-----------------------------------")
-// console.log(app.use(express.static(__dirname)));
 
-//////////////////////////
-////// Actual code //////
-/////////////////////////
+/////////////////////////////////
+///Server/Database Connection///
+///////////////////////////////
 
 
 //Connect to dabase and throw error if present
@@ -60,9 +42,67 @@ db.on('error', function(err) {
 	console.log('Database Error: ', err);
 });
 
-//Use the controller file to handle the routing
-app.get('/', routes);
-
 app.listen(PORT, function() {
 	console.log('App running on port 3000!');
 });
+
+
+//////////////////////////
+//////// Routes /////////
+////////////////////////
+
+// Root
+app.get('/', function(req,res) {
+  res.send(index.html);
+});
+ 
+// Retrieve all articles
+app.get('/articles', function(req, res){
+	Article.find({}, function(err, doc){
+		if (err){
+			console.log(err);
+		} else {
+			res.json(doc);
+		}
+	});
+});
+
+// Viewing previous comments
+app.get('/articles/:id', function(req, res){
+	Article.findOne({'_id': req.params.id})
+	.populate('comment')
+	.exec(function(err, doc){
+		if (err){
+			console.log(err);
+		} else {
+			res.json(doc);
+		}
+	});
+});
+
+
+// Posting a comment
+app.post('/save', function(req, res){
+	var newComment = new Comment(req.body);
+
+	newComment.save(function(err, doc){
+		if(err){
+			console.log(err);
+		} else {
+			Article.findOneAndUpdate({'_id': req.params.id}, {'note':doc._id})
+			.exec(function(err, doc){
+				if (err){
+					console.log(err);
+				} else {
+					res.send(doc);
+				}
+			});
+
+		}
+	});
+});
+
+
+// app.delete('/delete', function(req, res){
+
+// });
